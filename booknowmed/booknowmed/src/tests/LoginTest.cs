671f73d238acdc145731d12a08c6e2e3f22a4df7
Ivica Nikolic <ivicanklc@gmail.com>
2021-01-09 18:32:04 +0100

    LoginTest class is created

---
+++ booknowmed/booknowmed/src/tests/LoginTest.cs
@@ -0,0 +1,30 @@
+﻿using booknowmed.src.pages;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booknowmed.src.tests
+{
+	[TestFixture]
+	public class LoginTest : BaseTest
+	{
+
+		[Test]
+		public void FirstTest()
+		{
+			LoginPage loginPage = new LoginPage(this.driver, this.wait);
+			MessagesPage messagesPage = new MessagesPage(this.driver, this.wait);
+
+			this.driver.Navigate().GoToUrl(this.baseUrl + "/dialysis/login");
+
+			loginPage.ClickOnCloseBtn();
+
+			var displayed = messagesPage.IsTitleDisplayed();
+
+			Assert.IsFalse(displayed, "Close button is not working");
+		}
+	}
+}
