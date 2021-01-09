a40e765656eddd92eee7015d64391db42e84722d
Ivica Nikolic <ivicanklc@gmail.com>
2021-01-09 13:04:51 +0100

    BaseTest class is created

--- booknowmed/booknowmed/src/tests/BaseTest.cs
+++ booknowmed/booknowmed/src/tests/BaseTest.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,41 @@ using System.Threading.Tasks;
 
 namespace booknowmed.src.tests
 {
-	class BaseTest
+	[TestFixture]
+	public abstract class BaseTest
 	{
+
+		protected IWebDriver driver;
+		protected WebDriverWait wait;
+		protected String baseUrl = "https://bnm.neopixdev.com";
+
+
+		[OneTimeSetUp]
+		public void SetUp()
+		{
+			this.driver = new FirefoxDriver();
+			this.wait = new WebDriverWait(this.driver, TimeSpan.FromSeconds(10));
+			this.driver.Manage().Timeouts().PageLoad = TimeSpan.FromSeconds(20);
+			this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+			this.driver.Manage().Window.Maximize();
+
+		}
+
+
+
+		[TearDown]
+		public void AfterMethod()
+		{
+			this.driver.Manage().Cookies.DeleteAllCookies();
+			this.driver.Navigate().Refresh();
+		}
+
+
+
+		[OneTimeTearDown]
+		public void TearDown()
+		{
+			this.driver.Quit();
+		}
 	}
 }
