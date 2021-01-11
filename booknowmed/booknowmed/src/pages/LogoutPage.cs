19bf131d24edf92a9d85b5fac860ac4b85f06724
Ivica Nikolic <ivicanklc@gmail.com>
2021-01-11 14:51:15 +0100

    Logout page is created

---
+++ booknowmed/booknowmed/src/pages/LogoutPage.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.Extensions;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace booknowmed.src.pages
+{
+	internal class LogoutPage : BasePage
+	{
+		public LogoutPage(IWebDriver driver, WebDriverWait wait) : base(driver, wait)
+		{
+		}
+
+
+		public void UserLogout()
+		{
+			//System.Threading.Thread.Sleep(2000);
+
+			this.driver.ExecuteJavaScript("arguments[0].click();", this.GetProfile());
+			this.driver.ExecuteJavaScript("arguments[0].click();", this.GetLogoutBtn());
+
+		}
+
+
+
+		//getters
+		public IWebElement GetProfile()
+		{
+			return this.driver.FindElement(By.XPath("//span[@class='display-name']"));
+		}
+
+
+		public IWebElement GetLogoutBtn()
+		{
+			return this.driver.FindElement(By.XPath("//button[normalize-space()='Log out']"));
+		}
+
+
+	}
+
+}
